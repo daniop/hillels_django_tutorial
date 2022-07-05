e24bfe4068d16808bec43d51d71f7cf4c14532c8
Danylo Tseselskyi <danylotseselskyi@gmail.com>
2022-07-05 13:44:39 +0300

    flake8 fixes

--- polls/management/commands/create_users.py
+++ polls/management/commands/create_users.py
@@ -1,11 +1,12 @@
-from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+
 from faker import Faker
 
 
 class Command(BaseCommand):
-    help = 'Create random users'
+    help = 'Create random users'  # noqa: A003
 
     def add_arguments(self, parser):
         parser.add_argument('total', choices=range(1, 11), type=int,
--- polls/management/commands/delete_users.py
+++ polls/management/commands/delete_users.py
@@ -1,9 +1,9 @@
-from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = 'Delete users with id you set'
+    help = 'Delete users with id you set'  # noqa: A003
 
     def add_arguments(self, parser):
         parser.add_argument('user_id', nargs='+', type=int,
@@ -13,4 +13,3 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         users_ids = kwargs['user_id']
         User.objects.filter(id__in=users_ids).delete()
-
